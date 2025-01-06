3599e70eb765d8c90a3b522581f9affa3c2481a8
AymericLD <aymericld@outlook.fr>
2025-01-06 12:30:01 +0100

    Modification in order to plot several spectra on the same figure

--- Main_Spec.py
+++ Main_Spec.py
@@ -9,20 +9,83 @@ from Particles import Evolution_Particles
 from Spectral_Analysis import Spectral_Particles_Analysis
 import time
 import numpy as np
+from numpy.typing import NDArray
+import matplotlib.pyplot as plt
+
 
 start_time = time.time()
+
+
+def velocity_spectrum_gamma(
+    psi_0: float,
+    A: float,
+    L: float,
+    width: float,
+    c_x: float,
+    time_step: float,
+    t_final: float,
+    particle_number: int,
+    f: float,
+    Gamma: NDArray,
+    savefig: bool,
+    filepath: str,
+):
+    plt.figure(figsize=(10, 6))
+    for g in Gamma:
+        Inertial_waves = Bower_StreamFunction_with_inertial_waves(
+            psi_0=psi_0, A=A, L=L, width=width, c_x=c_x, f=f, gamma=g
+        )
+        Particles = Evolution_Particles(
+            stream_function=Inertial_waves,
+            time_step=time_step,
+            t_final=t_final,
+            particle_number=particle_number,
+            x_min=0,
+            x_max=2 * Inertial_waves.L,
+            y_min=-250,
+            y_max=250,
+        )
+        Spec = Spectral_Particles_Analysis(
+            time_step=time_step,
+            t_final=t_final,
+            num_particles=particle_number,
+            f=f,
+        )
+        u, v = Particles.velocity_profiles
+        Spec.plot_velocity_spectrum(u, v, filepath=filepath, savefig=2, f=f, gamma=g)
+        # savefig=2 is used for plotting several spectra on the same figure
+    plt.xlabel("Frequency (d^-1)")
+    plt.ylabel("Spectral Density")
+    plt.title("Velocity Spectral Density in frequency with varying gamma")
+    plt.grid(True, which="both", linestyle="--", linewidth=0.5)
+    plt.legend(fontsize=12)
+    plt.tight_layout()
+    if savefig:
+        plt.savefig(
+            f"{filepath}Velocity_Spectrum,f={f}gamma={Gamma}num_particles={particle_number}.png"
+        )
+    else:
+        plt.show()
 
 
 def main() -> None:
     # Backup Folder
 
     filepath = "Plots/Bower_Flow/Spectrum/"
+    savefig = False
 
     # Parameters
 
-    time_step = 1 / 8
+    time_step = 1 / 24 / 60
     t_final = 10
-    particle_number = 10
+    particle_number = 100
+    psi_0 = 4e3
+    A = 50
+    L = 400
+    width = 40
+    c_x = 10
+    f = 8.64
+    gamma = 100
 
     # Model
 
@@ -36,7 +99,7 @@ def main() -> None:
     Cell_Flow = Cellular_Flow(epsilon=0, L=100, alpha=10, phi=0)
 
     Inertial_waves = Bower_StreamFunction_with_inertial_waves(
-        psi_0=4e3, A=50, L=400, width=40, c_x=10, f=8.64, gamma=0.01
+        psi_0=psi_0, A=A, L=L, width=width, c_x=c_x, f=f, gamma=gamma
     )
 
     # Trajectories
@@ -66,13 +129,36 @@ def main() -> None:
     # Spectral Analysis
 
     Spec = Spectral_Particles_Analysis(
-        time_step=time_step, t_final=t_final, num_particles=particle_number
+        time_step=time_step, t_final=t_final, num_particles=particle_number, f=f
     )
 
     u_1, v_1 = Particles_1.velocity_profiles
     u_2, v_2 = Particles_2.velocity_profiles
-    Spec.plot_velocity_spectrum(u_2, v_2)
-    Spec.plot_velocity_spectrum(u_1, v_1)
+    # Spec.plot_velocity_spectrum(
+    #     u_2, v_2, filepath=filepath, savefig=1, f=f, gamma=gamma
+    # )
+    # Spec.plot_velocity_spectrum(
+    #     u_1, v_1, filepath=filepath, savefig=1, f=f, gamma=gamma
+    # )
+
+    # Spectra for varying gamma
+
+    Gamma = np.array([1e-3, 1e-2, 1e-1, 1])
+
+    velocity_spectrum_gamma(
+        psi_0=psi_0,
+        A=A,
+        L=L,
+        width=width,
+        c_x=c_x,
+        time_step=time_step,
+        t_final=t_final,
+        particle_number=particle_number,
+        f=f,
+        Gamma=Gamma,
+        savefig=savefig,
+        filepath=filepath,
+    )
 
     end_time = time.time()
 
--- Spectral_Analysis.py
+++ Spectral_Analysis.py
@@ -12,6 +12,7 @@ class Spectral_Particles_Analysis:
     time_step: float
     t_final: float
     num_particles: int
+    f: float
 
     def velocity_spectrum(self, u, v):
         N = int(self.t_final / self.time_step) - 1
@@ -48,29 +49,33 @@ class Spectral_Particles_Analysis:
         return u_spec, v_spec, vel_spec, freq
 
     def plot_velocity_spectrum(
-        self, u, v, filepath: str, savefig: bool, f: float, gamma: float
+        self, u, v, filepath: str, savefig: int, f: float, gamma: float
     ):
         """f is the frequency of the internal waves and gamma is the modulation of the geostrophic current"""
         """ f=0 and gamma=0 for the geostrophic current"""
+        """ savefig=0 : figure saved, savefig=1 : figure shown, savefig=2 : no output (used when plotting several spectrum in the same figure)"""
 
         u_spec, v_spec, vel_spec, freq = self.velocity_spectrum(u, v)
-        plt.figure(figsize=(10, 6))
 
-        plt.loglog(freq, vel_spec, label="Total")
-        plt.loglog(freq, u_spec, label="Zonal")
-        plt.loglog(freq, v_spec, label="Meridional")
+        if savefig != 2:
+            plt.figure(figsize=(10, 6))
 
-        plt.xlabel("Frequency (d^-1)")
-        plt.ylabel("Spectral Density")
-        plt.title("Velocity Spectral Density in frequency")
-        plt.grid(True, which="both", linestyle="--", linewidth=0.5)
-        plt.legend(fontsize=12)
-        plt.tight_layout()
+        # plt.loglog(freq, vel_spec, label="Total")
+        # plt.loglog(freq, u_spec, label="Zonal")
+        plt.loglog(freq, v_spec, label=f"Meridional,gamma={gamma}")
+        plt.plot([f / (2 * np.pi), f / (2 * np.pi)], [0, 1e12])
 
-        if savefig:
+        if savefig != 2:
+            plt.xlabel("Frequency (d^-1)")
+            plt.ylabel("Spectral Density")
+            plt.title("Velocity Spectral Density in frequency")
+            plt.grid(True, which="both", linestyle="--", linewidth=0.5)
+            plt.legend(fontsize=12)
+            plt.tight_layout()
+
+        if savefig == 0:
             plt.savefig(
                 f"{filepath}Velocity_Spectrum,f={f}gamma={gamma}num_particles={self.num_particles}.png"
             )
-
-        else:
+        elif savefig == 1:
             plt.show()
